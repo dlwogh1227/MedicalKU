8c08046423cb8aa3d7550f81ce4110341f9ef3e0
dlwogh1227 <dlwogh1227@naver.com>
2024-09-23 18:12:55 +0900

    first commit

---
+++ src/main/java/com/example/medicalku/MedicalKuApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalKuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalKuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medicalku/MedicalKuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalKuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
