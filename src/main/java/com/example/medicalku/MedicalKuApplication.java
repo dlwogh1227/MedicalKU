68363fad0bc5612b807b3efa4bc5561be9d701c3
dlwogh1227 <dlwogh1227@naver.com>
2024-10-03 19:32:34 +0900

    first commit

--- src/main/java/com/example/medicalku/MedicalKuApplication.java
+++ src/main/java/com/example/medicalku/MedicalKuApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedicalKuApplication {
-    //이재호
 
     public static void main(String[] args) {
         SpringApplication.run(MedicalKuApplication.class, args);
