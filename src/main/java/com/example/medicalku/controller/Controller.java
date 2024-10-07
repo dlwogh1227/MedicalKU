696a49361be095e7413bd8563328d5bb6b5597b5
dlwogh1227 <dlwogh1227@naver.com>
2024-10-07 20:36:33 +0900

    first commit

--- src/main/java/com/example/medicalku/controller/Controller.java
+++ src/main/java/com/example/medicalku/controller/Controller.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
